0ed318cc105f492e597e7e6bee2ba3657954b176
Edward <onirotto7585@pm.me>
2024-07-23 09:21:48 +0700

    encrypt finished

---
+++ main.py
@@ -0,0 +1,21 @@
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+# direction = input("Type 'encode' to encrypt or 'decode' to decrypt:\n")
+# user_text = input('Type your message:\n')
+# user_shift = int(input('Type your shift number:\n'))
+
+def encrypt(plain_text, shift_amount):
+    encrypted_text = ''
+
+    for letter in plain_text:
+        position = 0
+        position += (alphabet.index(letter) + shift_amount)
+
+        if position > 25:
+            position -= 26
+            
+        encrypted_text += alphabet[position]
+
+    print(encrypted_text)
+
+encrypt(shift_amount = 5, plain_text='zulu')
